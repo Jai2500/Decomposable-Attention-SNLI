fa72662cbfff78ae980fe17f6f1769b8498c1d14
Ahish9009 <ahishjayant9009@gmail.com>
2021-12-11 20:51:00 +0530

    formatted code

--- preprocess/get_pretrain_vecs.py
+++ preprocess/get_pretrain_vecs.py
@@ -5,9 +5,10 @@ import sys
 import operator
 import argparse
 
+
 def load_glove_vec(fname, vocab):
     word_vecs = {}
-    for line in open(fname, 'r'):
+    for line in open(fname, "r"):
         d = line.split()
         word = d[0]
         vec = np.array(list(map(float, d[1:])))
@@ -16,32 +17,41 @@ def load_glove_vec(fname, vocab):
             word_vecs[word] = vec
     return word_vecs
 
+
 def main():
-  parser = argparse.ArgumentParser(
-      description =__doc__,
-      formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument('--dictionary', help="*.dict file", type=str,
-                      default='data/entail.word.dict')
-  parser.add_argument('--glove', help='pretrained word vectors', type=str, default='/scratch/a9/glove/glove.6B.300d.txt')
-  parser.add_argument('--outputfile', help="output hdf5 file", type=str,
-                      default='../data/glove.hdf5')
-  
-  args = parser.parse_args()
-  print(args.dictionary)
-  vocab = open(args.dictionary, "r").read().split("\n")[:-1]
-  vocab = list(map(lambda x: (x.split()[0], int(x.split()[1])), vocab))
-  word2idx = {x[0]: x[1] for x in vocab}
-  print("vocab size is " + str(len(vocab)))
-  w2v_vecs = np.random.normal(size = (len(vocab), 300))
-  w2v = load_glove_vec(args.glove, word2idx)
-      
-  print("num words in pretrained model is " + str(len(w2v)))
-  for word, vec in w2v.items():
-      w2v_vecs[word2idx[word] - 1 ] = vec
-  for i in range(len(w2v_vecs)):
-      w2v_vecs[i] = w2v_vecs[i] / np.linalg.norm(w2v_vecs[i])
-  with h5py.File(args.outputfile, "w") as f:
-    f["word_vecs"] = np.array(w2v_vecs)
-    
-if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--dictionary", help="*.dict file", type=str, default="data/entail.word.dict"
+    )
+    parser.add_argument(
+        "--glove",
+        help="pretrained word vectors",
+        type=str,
+        default="/scratch/a9/glove/glove.6B.300d.txt",
+    )
+    parser.add_argument(
+        "--outputfile", help="output hdf5 file", type=str, default="../data/glove.hdf5"
+    )
+
+    args = parser.parse_args()
+    print(args.dictionary)
+    vocab = open(args.dictionary, "r").read().split("\n")[:-1]
+    vocab = list(map(lambda x: (x.split()[0], int(x.split()[1])), vocab))
+    word2idx = {x[0]: x[1] for x in vocab}
+    print("vocab size is " + str(len(vocab)))
+    w2v_vecs = np.random.normal(size=(len(vocab), 300))
+    w2v = load_glove_vec(args.glove, word2idx)
+
+    print("num words in pretrained model is " + str(len(w2v)))
+    for word, vec in w2v.items():
+        w2v_vecs[word2idx[word] - 1] = vec
+    for i in range(len(w2v_vecs)):
+        w2v_vecs[i] = w2v_vecs[i] / np.linalg.norm(w2v_vecs[i])
+    with h5py.File(args.outputfile, "w") as f:
+        f["word_vecs"] = np.array(w2v_vecs)
+
+
+if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -11,30 +11,86 @@ from src.data_models import w2v
 from pytorch_lightning.loggers import WandbLogger
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--train_file', type=str, default='data/entail-train.hdf5', help='training data file (hdf5)')
-parser.add_argument('--val_file', type=str, default='data/entail-val.hdf5', help='validation data file (hdf5)')
-parser.add_argument('--test_file', type=str, default='data/entail-test.hdf5', help='test data file (hdf5)')
-parser.add_argument('--max_length', type=int, default=82, help='maximum length of training sentences. -1 means no max length')
-parser.add_argument('--w2v_file', type=str, default='data/w2v.hdf5', help='pretrained word vectors file (hdf5)')
+parser.add_argument(
+    "--train_file",
+    type=str,
+    default="data/entail-train.hdf5",
+    help="training data file (hdf5)",
+)
+parser.add_argument(
+    "--val_file",
+    type=str,
+    default="data/entail-val.hdf5",
+    help="validation data file (hdf5)",
+)
+parser.add_argument(
+    "--test_file",
+    type=str,
+    default="data/entail-test.hdf5",
+    help="test data file (hdf5)",
+)
+parser.add_argument(
+    "--max_length",
+    type=int,
+    default=82,
+    help="maximum length of training sentences. -1 means no max length",
+)
+parser.add_argument(
+    "--w2v_file",
+    type=str,
+    default="data/w2v.hdf5",
+    help="pretrained word vectors file (hdf5)",
+)
 
-parser.add_argument('--embedding_size', type=int, default=300, help='word embedding size')
-parser.add_argument('--hidden_size', type=int, default=300, help='hidden layer size')
-parser.add_argument('--intra_sent_atten', type=bool, default=False, help='whether to use intra sentence attention')
+parser.add_argument(
+    "--embedding_size", type=int, default=300, help="word embedding size"
+)
+parser.add_argument("--hidden_size", type=int, default=300, help="hidden layer size")
+parser.add_argument(
+    "--intra_sent_atten",
+    type=bool,
+    default=False,
+    help="whether to use intra sentence attention",
+)
 
-parser.add_argument('--epoch', type=int, default=250, help='number of training epochs')
-parser.add_argument('--gpus', type=int, default=0, help='number of gpus to train on. -1 for all gpus')
-parser.add_argument('--val_interval', type=int, default=500, help='interval for checking the validation dataset')
-parser.add_argument('--num_workers', type=int, default=10, help='number of workers in the dataloader')
+parser.add_argument("--epoch", type=int, default=250, help="number of training epochs")
+parser.add_argument(
+    "--gpus", type=int, default=0, help="number of gpus to train on. -1 for all gpus"
+)
+parser.add_argument(
+    "--val_interval",
+    type=int,
+    default=500,
+    help="interval for checking the validation dataset",
+)
+parser.add_argument(
+    "--num_workers", type=int, default=10, help="number of workers in the dataloader"
+)
 
-parser.add_argument('--optimizer', type=str, default='adagrad', choices=['adam', 'adagrad'])
-parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
-parser.add_argument('--max_grad_norm', type=float, default=5, help='if the norm of the gradient vector exceeds this renormalize it to have the norm equal to the max_grad_norm')
-parser.add_argument('--param_init', type=float, default=0.01, help='parameter initialization gaussian')
-parser.add_argument('--weight_decay', type=float, default=5e-5, help='l2 regularization')
+parser.add_argument(
+    "--optimizer", type=str, default="adagrad", choices=["adam", "adagrad"]
+)
+parser.add_argument("--lr", type=float, default=0.05, help="learning rate")
+parser.add_argument(
+    "--max_grad_norm",
+    type=float,
+    default=5,
+    help="if the norm of the gradient vector exceeds this renormalize it to have the norm equal to the max_grad_norm",
+)
+parser.add_argument(
+    "--param_init", type=float, default=0.01, help="parameter initialization gaussian"
+)
+parser.add_argument(
+    "--weight_decay", type=float, default=5e-5, help="l2 regularization"
+)
 
-parser.add_argument('--log_dir', type=str, default='logs/', help='log file directory')
-parser.add_argument('--model_path', type=str, default='model', help='path of the model (w/o suffix)')
-parser.add_argument('--use_wandb', type=bool, default=False, help='whether to use wandb for logging')
+parser.add_argument("--log_dir", type=str, default="logs/", help="log file directory")
+parser.add_argument(
+    "--model_path", type=str, default="model", help="path of the model (w/o suffix)"
+)
+parser.add_argument(
+    "--use_wandb", type=bool, default=False, help="whether to use wandb for logging"
+)
 
 args = parser.parse_args()
 TRAIN_LBL_SIZE = 3
@@ -44,7 +100,7 @@ datamodule = LitSNLI(
     val_file=args.val_file,
     test_file=args.test_file,
     max_length=args.max_length,
-    n_workers=args.num_workers
+    n_workers=args.num_workers,
 )
 
 word_vecs = w2v(args.w2v_file).word_vecs
@@ -61,9 +117,7 @@ encoder.embedding.weight.data.copy_(word_vecs)
 encoder.embedding.weight.requires_grad = False
 
 atten = Atten(
-    hidden_size=args.hidden_size,
-    label_size=TRAIN_LBL_SIZE,
-    param_init=args.param_init
+    hidden_size=args.hidden_size, label_size=TRAIN_LBL_SIZE, param_init=args.param_init
 )
 
 model = LitModel(
@@ -72,28 +126,28 @@ model = LitModel(
     max_grad_norm=args.max_grad_norm,
     lr=args.lr,
     optim=args.optimizer,
-    weight_decay=args.weight_decay
+    weight_decay=args.weight_decay,
 )
 
 if args.use_wandb:
     wandb_logger = WandbLogger(project="ANLP-Project")
 
-checkpoint_callback = ModelCheckpoint( 
-  monitor='Validation Loss', 
-  dirpath='./temp/', 
-  filename='anlp-model-{epoch:02d}',
-  save_top_k = 3,
-  mode='min'
+checkpoint_callback = ModelCheckpoint(
+    monitor="Validation Loss",
+    dirpath="./temp/",
+    filename="anlp-model-{epoch:02d}",
+    save_top_k=3,
+    mode="min",
 )
 
 trainer = pl.Trainer(
     max_epochs=args.epoch,
     gpus=args.gpus,
-    accelerator='cpu' if args.gpus == 0 else 'ddp',
+    accelerator="cpu" if args.gpus == 0 else "ddp",
     progress_bar_refresh_rate=10,
     val_check_interval=args.val_interval,
     logger=wandb_logger if args.use_wandb else None,
-    callbacks=[checkpoint_callback]
+    callbacks=[checkpoint_callback],
 )
 
 trainer.fit(
